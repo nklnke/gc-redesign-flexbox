50d86a179fa1e502482deb3d60b12fc46a69fe37
Danil Kozlov <megacokes@gmail.com>
2018-06-20 21:39:57 +0500

    cart

--- js/scripts.js
+++ js/scripts.js
@@ -6,7 +6,7 @@ function myFunction() {
 window.onclick = function(event) {
 if (!event.target.matches('.dropbtn')) {
 
-  var dropdowns = document.getElementsByClassName("dropdown-content");
+  var dropdowns = document.getElementsByClassName("cart-content");
   var i;
   for (i = 0; i < dropdowns.length; i++) {
     var openDropdown = dropdowns[i];
