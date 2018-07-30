93064e23c7924c75c3e47b084bfbd41a5793aa66
Danil Kozlov <megacokes@gmail.com>
2018-07-30 20:19:44 +0500

    wordpress carcass

---
+++ wordpress-theme/footer.php
@@ -0,0 +1,66 @@
+</main>
+    <!-- /content -->
+
+  </div>
+  <!-- /container -->
+
+  <footer class="flex">
+  
+    <div class="dark">
+      
+      <!-- SnapWidget -->
+      <iframe src="https://snapwidget.com/embed/576342" class="snapwidget-widget" allowtransparency="true" frameborder="0" scrolling="no" style="border:none; overflow:hidden; width:220px; height:220px"></iframe>
+
+    </div>
+
+    <div class="light">
+
+      <!-- VK Widget -->
+      <div id="vk_groups"></div>
+      <script type="text/javascript">
+      VK.Widgets.Group("vk_groups", {mode: 3, width: "300", color1: 'EFB03B', color2: 'FFFFFF', color3: 'FFFFFF'}, 61616920);
+      </script>
+
+    </div>
+
+    <div class="dark">
+      <p>
+        <a href="">Комиксы</a>
+        <a href="">Фигурки</a>
+        <a href="">Артбуки</a>
+        <a href="">Зины</a>
+        <a href="" class="accent-link">Подарочные сертификаты</a>
+        <a href="">Оплата и доставка</a>
+        <a href="">Контакты</a>
+        <a href="">Пользовательское соглашение</a>
+      </p>
+    </div>
+
+    <div class="light">
+      <p>
+        <a href="">
+          <img src="img/icons/map.png" alt="Map"> Екатеринбург<br>
+          ул. Бориса Ельцина 3<br>
+          Ельцин-центр, 2 этаж
+        </a>
+        <a href="" class="accent-link"><img src="img/icons/tel.png" alt="tel"> 8 (999) 454 6425</a>
+        <a href=""><img src="img/icons/mail.png" alt="mail"> support@gotcomics.ru</a>
+      </p>
+    </div>
+  
+  </footer>
+
+  <!-- vk chat 
+  <div id="vk_community_messages"></div>-->
+
+  <script src="js/scripts.js"></script>
+
+</body>
+</html>
+
+<!--
+  TODO:
+    - !!! aside последний блок сделать на всю высоту, чтобы не было белой дырки
+    - сделать спрайты для иконок
+    - перевести иконки в png
+-->
---
+++ wordpress-theme/header.php
@@ -0,0 +1,172 @@
+<!DOCTYPE html>
+<html lang="ru">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <meta http-equiv="X-UA-Compatible" content="ie=edge">
+  <title>Got Comics 📙 Магазин комиксов в Екатеринбурге</title>
+  <link rel="stylesheet" href="css/normalize.min.css">
+  <link rel="stylesheet" href="css/grid.css">
+  <link rel="stylesheet" href="css/style.css">
+  <link rel="stylesheet" href="css/product-page.css">
+  <script type="text/javascript" src="https://vk.com/js/api/openapi.js?157"></script>
+</head>
+<body>
+
+  <div class="container flex">
+    
+    <aside>
+
+      <div class="logo dark center">
+        <a href="index.html">
+          <img src="img/gcexplosion.png" alt="got comics">
+        </a>
+      </div>
+
+      <div class="sidebar-menu">
+
+        <nav class="light">
+          <a class="first-sidebar-item"><span>Переплёт</span></a>
+          <a href=""><img src="img/icons/single.png"><span>Синглы</span></a>
+          <a href=""><img src="img/icons/tpb.png"><span>Мягкий переплёт</span></a>
+          <a href=""><img src="img/icons/hc.png"><span>Твёрдый переплёт</span></a>
+        </nav>
+        
+        <nav class="dark">
+          <a class="first-sidebar-item"><span>Издательства</span></a>
+          <a href=""><img src="img/publ/42.svg" alt="Издательство 42"><span>42</span></a>
+          <a href=""><img src="img/publ/alt-graph.svg" alt="Alt Graph"><span>Alt Graph</span></a>
+          <a href=""><img src="img/publ/bubble.svg" alt="Bubble"><span>Bubble</span></a>
+          <a href=""><img src="img/publ/jellyfish-jam.svg" alt="Jellyfish Jam"><span>Jellyfish Jam</span></a>
+          <a href=""><img src="img/publ/illusion-studios.svg" alt="Illusion studios"><span>Illusion Studios</span></a>
+          <a href=""><img src="img/publ/panini-rus.svg" alt="Panini Rus"><span>Panini Rus</span></a>
+          <a href=""><img src="img/publ/parallel-comics.svg" alt="Parallel Comics"><span>Parallel Comics</span></a>
+          <a href=""><img src="img/publ/smart-owl.svg" alt="Smart Owl"><span>Smart Owl</span></a>
+          <a href=""><img src="img/publ/vd.svg" alt="VD Pudlishing"><span>VD Publishing</span></a>
+          <a href=""><img src="img/publ/xlmedia.svg" alt="XL Media"><span>XL Media</span></a>
+          <a href=""><img src="img/publ/zangavar.svg" alt="Zangavar"><span>Zangavar</span></a>
+          <a href=""><img src="img/publ/azbuka.svg" alt="Азбука-Аттикус"><span>Азбука-Аттикус</span></a>
+          <a href=""><img src="img/publ/ast.svg" alt="АСТ"><span>АСТ</span></a>
+          <a href=""><img src="img/publ/bumkniga.svg" alt="Бумкнига"><span>Бумкнига</span></a>
+          <a href=""><img src="img/publ/unicorn.svg" alt="Белый единорог"><span>Белый Единорог</span></a>
+          <a href=""><img src="img/publ/kkf.svg" alt="Книжный клуб Фантастика"><span>КК Фантастика</span></a>
+          <a href=""><img src="img/publ/komilfo.svg" alt="Комильфо"><span>Комильфо</span></a>
+          <a href=""><img src="img/publ/comfed.svg" alt="КомФедерация"><span>КомФедерация</span></a>
+          <a href=""><img src="img/publ/ramona.svg" alt="Рамона"><span>Рамона</span></a>
+          <a href=""><img src="img/publ/eksmo.svg" alt="Эксмо"><span>Эксмо</span></a>
+          <a href=""><img src=""><span>Прочие издательства</span></a>
+        </nav>
+
+        <nav class="light">
+          <a class="first-sidebar-item"><span>Фигурки</span></a>
+          <a href=""><img src="img/publ/funko.png"><span>Funko</span></a>
+          <a href=""><img src="img/publ/kidrobot.png"><span>Kidrobot</span></a>
+          <a href=""><span>Все фигурки</span></a>
+        </nav>
+          
+        <nav class="dark">
+          <a class="first-sidebar-item1"><span>Зины</span></a>
+          <a href=""><span>Артбуки</span></a>
+          <a href=""><span>Разное</span></a>
+        </nav>
+
+        <nav class="light">
+          <a class="first-sidebar-item"><span>Популярное</span></a>
+          <a href=""><img src="img/publ/batman.png"><span>Бэтмен</span></a>
+          <a href=""><img src="img/publ/superman.png"><span>Супермен</span></a>
+          <a href=""><img src="img/publ/deadpool.png"><span>Дэдпул</span></a>
+          <a href=""><span>Бэтмен</span></a>
+          <a href=""><span>Супермен</span></a>
+          <a href=""><span>Дэдпул</span></a>
+          <a href=""><span>link</span></a>
+          <a href=""><span>link</span></a>
+        </nav>
+
+        <nav class="dark">
+          <a class="first-sidebar-item"><span>Link</span></a>
+          <a href=""><span>Link</span></a>
+        </nav>
+        
+        <nav class="light">
+          <a class="first-sidebar-item"><span>Link</span></a>
+          <a href=""><span>Link</span></a>
+        </nav>
+
+        <nav class="dark">
+          <a class="first-sidebar-item"><span>Link</span></a>
+          <a href=""><span>Link</span></a>
+        </nav>
+        
+        <nav class="light">
+          <a class="first-sidebar-item"><span>Link</span></a>
+          <a href=""><span>Link</span></a>
+        </nav>
+        
+        <nav class="dark">
+          <a class="first-sidebar-item"><span>Link</span></a>
+          <a href=""><span>Link</span></a>
+        </nav>
+        
+        <nav class="light">
+          <a class="first-sidebar-item"><span>Link</span></a>
+          <a href=""><span>Link</span></a>
+        </nav>
+
+      </div>
+
+    </aside>
+    <!-- /sidebar -->
+
+    <main>
+
+      <div class="header flex">
+
+        <div class="header-item flex light">
+          <span>Магазин комиксов и всяких штук</span>
+          <span>
+            <img src="img/icons/map.png" alt="Map">
+            <a href="">ул. Бориса Ельцина 3, Ельцин-центр, 2 этаж</a>
+          </span>
+          <span>
+            <img src="img/icons/tel.png" alt="tel">
+            <a href="tel:+7-999-514-9555">8 (999) 514 9555</a>
+            <img src="img/icons/mail.png" alt="mail">
+            <a href="mailto:support@gotcomics.ru">support@gotcomics.ru</a>
+          </span>
+        </div>
+
+        <div class="header-item flex dark">
+          <span>
+            <a href="">Оплата/доставка</a>
+          </span>
+          <span>
+            <a href="">Подарочные сертификаты</a>
+          </span>
+          <span>
+            <a href="">Контакты</a>
+          </span>
+        </div>
+
+        <div class="header-item flex light cart">
+          <a href="">
+            <img src="img/icons/cart.svg" alt="Корзина">
+            <p>
+              <span>Моя корзина</span>
+              <span>2 шт. на сумму 1720р</span>
+            </p>
+          </a>
+        </div>
+
+      </div>
+      <!-- /menu -->
+
+      <div class="breadcrumbs">
+
+        <p>Упорядочить по:</p>
+        <p><a href="#">новизне</a></p>
+        <p><a class="breadcrumbs-active" href="#">названию</a></p>
+        <p><a href="#">цене</a></p>
+        <input class="search-form" type="search" placeholder="Поиск по сайту">
+
+      </div>
+      <!-- /breadcrumbs -->
