7b1c207b5e3b9f1948e4073d6cf698cf873125ff
Danil Kozlov <megacokes@gmail.com>
2018-07-31 10:31:20 +0500

    functions.php css/js addin

--- wordpress-theme/functions.php
+++ wordpress-theme/functions.php
@@ -1,1 +1,15 @@
-add_theme_support('woocommerce');
+add_theme_support( 'woocommerce' );
+
+// css register
+wp_register_style( 'normalize', get_template_directory_uri() . '/css/normalize.min.css', array(), 'screen');
+wp_register_style( 'style', get_template_directory_uri() . '/css/style.css', array('normalize'), 'screen');
+
+// css connection
+wp_enqueue_style( 'normalize');
+wp_enqueue_style( 'style');
+
+// js register
+wp_register_script( 'jquery', 'http://ajax.googleapis.com/ajax/libs/jquery/1.11.0/jquery.min.js', array(), false);
+    
+// js connection
+wp_enqueue_script( 'jquery');
