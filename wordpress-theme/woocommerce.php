656678d055f93aa43b22756cf4860028691bdf2a
Danil Kozlov <megacokes@gmail.com>
2018-08-01 19:44:06 +0500

    woocommerce.php lil progress

--- wordpress-theme/woocommerce.php
+++ wordpress-theme/woocommerce.php
@@ -1,1 +1,24 @@
-<?php woocommerce_content(); ?>
+<!DOCTYPE html>
+<html lang="ru">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <meta http-equiv="X-UA-Compatible" content="ie=edge">
+  <title>Got Comics рџ“™ РњР°РіР°Р·РёРЅ РєРѕРјРёРєСЃРѕРІ РІ Р•РєР°С‚РµСЂРёРЅР±СѓСЂРіРµ</title>
+  
+  <link rel="stylesheet" href="<?php echo get_stylesheet_directory_uri() ?>/css/normalize.min.css">
+  <link rel="stylesheet" href="<?php echo get_stylesheet_directory_uri() ?>/css/grid.css">
+  <link rel="stylesheet" href="<?php echo get_stylesheet_directory_uri() ?>/style.css">
+  <link rel="stylesheet" href="<?php echo get_stylesheet_directory_uri() ?>/css/product-page.css">
+  <script type="text/javascript" src="https://vk.com/js/api/openapi.js?157"></script>
+</head>
+<body <?php body_class(); ?>>
+
+  <div class="container flex">
+
+	<?php get_header(); ?>
+	 
+	<?php woocommerce_content(); ?>
+
+	<?php get_sidebar(); ?>
+	<?php get_footer(); ?>
